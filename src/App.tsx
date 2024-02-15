87ace0fedf4686dee3b94be7f87ca398baa2b22e
Whitney Waldinger <wwaldi@uw.edu>
2024-02-15 13:06:41 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import {
   PagingInfo,
   ResultsPerPage,
   Paging,
-  WithSearch
+  WithSearch,
+  Facet // Import Facet component
 } from "@elastic/react-search-ui";
 import {
   Layout,
@@ -34,13 +35,88 @@ const config: SearchDriverOptions = {
   hasA11yNotifications: true,
   searchQuery: {
     result_fields: {
-      title: { raw: {} }
+      title: {
+        snippet: {
+          fallback: true
+        }
+      },
+      description: {
+        snippet: {
+          fallback: true
+        }
+      },
+      states: {
+        snippet: {
+          fallback: true
+        }
+      },
+      visitors: { raw: {} },
+      acres: { raw: {} },
+      square_km: { raw: {} },
+      date_established: { raw: {} },
+      nps_link: { raw: {} },
     },
     search_fields: {
-      title: {}
+      title: {
+        weight: 5
+      },
+      description: {},
+      states: {}
     },
-    disjunctiveFacets: [""],
-    facets: {}
+    disjunctiveFacets: ["states"],
+    facets: {
+      states: { type: "value", size: 30 },
+      acres: {
+        type: "range",
+        ranges: [
+          { from: -1, name: "Any" },
+          { from: 0, to: 1000, name: "Small" },
+          { from: 1001, to: 100000, name: "Medium" },
+          { from: 100001, name: "Large" }
+        ]
+      },
+      location: {
+        // San Francisco. In the future, make this the user's current position
+        center: "37.7749, -122.4194",
+        type: "range",
+        unit: "mi",
+        ranges: [
+          { from: 0, to: 100, name: "Nearby" },
+          { from: 100, to: 500, name: "A longer drive" },
+          { from: 500, name: "Perhaps fly?" }
+        ]
+      },
+      date_established: {
+        type: "range",
+        ranges: [
+          {
+            from: '1972-04-13T12:48:33.420Z',
+            name: "Within the last 50 years"
+          },
+          {
+            from: '1922-04-13T12:48:33.420Z',
+            to: '1972-04-13T12:48:33.420Z',
+            name: "50 - 100 years ago"
+          },
+          {
+            to: '1922-04-13T12:48:33.420Z',
+            name: "More than 100 years ago"
+          }
+        ]
+      },
+      visitors: {
+        type: "range",
+        ranges: [
+          { from: 0, to: 10000, name: "0 - 10000" },
+          { from: 10001, to: 100000, name: "10001 - 100000" },
+          { from: 100001, to: 500000, name: "100001 - 500000" },
+          { from: 500001, to: 1000000, name: "500001 - 1000000" },
+          { from: 1000001, to: 5000000, name: "1000001 - 5000000" },
+          { from: 5000001, to: 10000000, name: "5000001 - 10000000" },
+          { from: 10000001, name: "10000001+" }
+        ]
+      }
+    }, 
   }
 };
 
@@ -49,7 +125,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">ACEP Capstone UI</h1>
         <img src="acep-logo.png" className="App-logo" alt="ACEP Logo" />
       </header>
 
@@ -75,6 +150,38 @@ function App() {
                           thumbnailField="image_url"
                           shouldTrackClickThrough
                         />
+                      }
+                      sideContent={ // Add sideContent here
+                        <div>
+                          <Facet
+                            field="states"
+                            label="States"
+                            isFilterable={true}
+                          />
+                          <Facet
+                            field="world_heritage_site"
+                            label="World Heritage Site?"
+                            //view={BooleanFacet}
+                          />
+                          <Facet
+                            field="visitors"
+                            label="Visitors"
+                            //view={SingleLinksFacet}
+                          />
+                          <Facet
+                            field="date_established"
+                            label="Date Established"
+                          />
+                          <Facet
+                            field="location"
+                            label="Distance"
+                          />
+                          <Facet
+                            field="acres"
+                            label="Acres"
+                            //view={SingleSelectFacet}
+                          />
+                        </div>
                       }
                       bodyHeader={
                         <React.Fragment>

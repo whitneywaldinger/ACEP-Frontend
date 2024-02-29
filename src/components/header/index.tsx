e65890420e48813cac3b9a5736e10709d21924fa
Whitney Waldinger <wwaldi@uw.edu>
2024-02-29 09:51:26 -0800

    Added github link

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,9 +5,9 @@ import {Link} from "react-router-dom";
 function Header(){
     return (
         <header className="App-header">
-            <Link className="App-documentation" to={"/docs"}>
-                <h1>View Documentation</h1>
-            </Link>
+            <a href="https://github.com/whitneywaldinger/ACEP-Frontend">
+                <img src="gh_logo.png" className="App-logo" alt="GitHub Logo" />
+            </a>
             <h1 className="App-title">ACEP Capstone UI</h1>
             <img src="acep-logo.png" className="App-logo" alt="ACEP Logo" />
         </header>

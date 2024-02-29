5e2ed1bd608a6df3db509d93a96a7249e67ddc9d
Whitney Waldinger <wwaldi@uw.edu>
2024-02-29 10:00:34 -0800

    Updated orientation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,10 +6,10 @@ function Header(){
     return (
         <header className="App-header">
             <a href="https://github.com/whitneywaldinger/ACEP-Frontend">
-                <img src="gh_logo.png" className="App-logo" alt="GitHub Logo" />
+                <img src="gh_logo.png" className="GH-logo" alt="GitHub Logo" />
             </a>
             <h1 className="App-title">ACEP Capstone UI</h1>
-            <img src="acep-logo.png" className="App-logo" alt="ACEP Logo" />
+            <img src="acep-logo.png" className="ACEP-logo" alt="ACEP Logo" />
         </header>
     );
 }
